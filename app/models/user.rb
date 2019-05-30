fd0ae0bb22238800a2139b23315a885ea3a4cea3
Mike Schutte <tmikeschutte@gmail.com>
2019-05-30 09:46:14 -0400

    Add User#active_tasks

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,14 @@ class User < ApplicationRecord
   has_many :users_houses
   has_many :tasks, through: :users_tasks
   has_many :houses, through: :users_houses
+
+  def active_tasks(week = this_week)
+    users_tasks.where(week_of: week)
+  end
+
+  private
+
+  def this_week
+    @_this_week ||= Date.today.at_beginning_of_week
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,4 +3,33 @@ require "rails_helper"
 RSpec.describe User, type: :model do
   it { should have_many(:houses).through(:users_houses) }
   it { should have_many(:tasks).through(:users_tasks) }
+
+  describe "#active_tasks" do
+    let(:user) { create(:user) }
+    let(:tasks) { create_list(:task, 3) }
+    let(:this_week) { Date.today.at_beginning_of_week }
+    let(:last_week) { this_week - 1.week }
+    let!(:new_tasks) {
+      tasks.map { |task|
+        create(:users_task, user: user, task: task, week_of: this_week)
+      }
+    }
+    let!(:old_tasks) {
+      tasks.map { |task|
+        create(:users_task, user: user, task: task, week_of: last_week)
+      }
+    }
+
+    it "returns the active tasks for the current week by default" do
+      active_tasks = user.active_tasks
+      expect(active_tasks).to match_array new_tasks
+      expect(active_tasks).to_not match_array user.users_tasks
+    end
+
+    it "returns the active tasks for a specified week" do
+      active_tasks = user.active_tasks(last_week)
+      expect(active_tasks).to match_array old_tasks
+      expect(active_tasks).to_not match_array user.users_tasks
+    end
+  end
 end
